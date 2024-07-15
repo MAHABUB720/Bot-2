14fc9ac589add1d372967265f769c9353fb05a7a
MAHABUB0712 <sadiyaxmahabur@gmail.com>
2024-07-15 19:01:14 +0600

    Update Xnxx140.js

--- scripts/commands/Xnxx140.js
+++ scripts/commands/Xnxx140.js
@@ -1,170 +1,28 @@
-const fs = global.nodemodule["fs-extra"];
+const request = require("request");
+const fs = require("fs")
+const axios = require("axios")
 module.exports.config = {
-  name: "xnxx140",
-  version: "1.0.1",
+  name: "bop",
+  version: "1.0.0",
   permssion: 0,
-  prefix: false,
-  credits: "Abdulla Rahaman",
-  description: "goibot",
-  category: "Noprefix",
-  usages: "noprefix",
+  prefix: true,
+  credits: "Kaneki",
+  description: "Bóp mông người bạn tag",
+  category: "Lệnh 18+",
+  usages: "[tag]",
   cooldowns: 5,
 };
-module.exports.handleEvent = async function({ api, event, args, Threads, Users }) {
-  var { threadID, messageID, reason } = event;
-  const moment = require("moment-timezone");
-  const time = moment.tz("Asia/Manila").format("HH:MM:ss L");
-  var idgr = `${event.threadID}`;
-  var id = event.senderID;
-  var name = await Users.getNameUser(event.senderID);
 
-  var tl = ["হ্যা বলো আমি শুনছি😌","আমি শুধু আমার বস সুমু কে ভালোবাসি 😺"];
-  var rand = tl[Math.floor(Math.random() * tl.length)]
-
-    if ((event.body.toLowerCase() == "kamon asen") || (event.body.toLowerCase() == "love u bot")) {
-     return api.sendMessage("Hmm bhalo asi apni🙂🤖", threadID);
-   };
-
-    if ((event.body.toLowerCase() == "ummah bot") || (event.body.toLowerCase() == "bot ummah")) {
-     return api.sendMessage("লুচ্চা চুম্মা চুম্মি করোস কেন🥵", threadID);
-   };
-   
-    if ((event.body.toLowerCase() == "i love u bot") || (event.body.toLowerCase() == "i love you bot")) {
-     return api.sendMessage("আমাকে না আমার বস সুমু কে ভালোবাসো😻🥀", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "baler bot") || (event.body.toLowerCase() == "fuck bot")) {
-     return api.sendMessage("কিরে আমাকে গালি দেস কেনো তোকে কিন্তু বেন করে দিমু😠", threadID);
-   };
-  
-   if ((event.body.toLowerCase() == "kiss bot") || (event.body.toLowerCase() == "bot kiss me")) {
-     return api.sendMessage("আমি ভালো তুমি পঁচা😌", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "bot koire") || (event.body.toLowerCase() == "bot koi")) {
-     return api.sendMessage("এই তো আমি এখানে🙋‍♂️", threadID);
-   };
-
-  if ((event.body.toLowerCase() == "bot tomar admin ke") || (event.body.toLowerCase() == "আমার এডমিন হলো মাহাবুর")) {
-     return api.sendMessage("", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "bsvv nha mn") || (event.body.toLowerCase() == "bsvv nha mn")) {
-     return api.sendMessage("Hello dear, have a nice day ❤️", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "btvv nha mng") || (event.body.toLowerCase() == "btvv nha mng")) {
-     return api.sendMessage("Hello dear, have a nice day ❤️", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "hi ae") || (event.body.toLowerCase() == "hi ae")) {
-     return api.sendMessage("Hello dear, have a nice day ❤️", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "hiii") || (event.body.toLowerCase() == "hiii")) {
-     return api.sendMessage("Hello dear, have a nice day ❤️", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "btvv nha mn") || (event.body.toLowerCase() == "btvv nha mn")) {
-     return api.sendMessage("Hello dear, have a nice day ❤️", threadID);
-   };
-
-
-   if ((event.body.toLowerCase() == "tt go mng") || (event.body.toLowerCase() == "tt go mng")) {
-     return api.sendMessage("️1 is interaction, 2 is kick :))))", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "let's go") || (event.body.toLowerCase() == "let's go")) {
-     return api.sendMessage("️1 is interaction, 2 is kick :))))", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "tt mng oi") || (event.body.toLowerCase() == "tt mng oi")) {
-     return api.sendMessage("️1 is interaction, 2 is kick :))))", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "nn nha mng") || (event.body.toLowerCase() == "nn nha mng")) {
-     return api.sendMessage("️Sleep well <3 Wish you all super nice dreams <3", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "tt go mn") || (event.body.toLowerCase() == "tt go mn")) {
-     return api.sendMessage("️1 is interaction, 2 is kick :))))", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "flop over") || (event.body.toLowerCase() == "flop over")) {
-     return api.sendMessage("️1 is interaction, 2 is kick :))))", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "clmm bot") || (event.body.toLowerCase() == "clmm bot")) {
-     return api.sendMessage("️Swear something dog :) you've been holding on to you for a long time", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "bot cc") || (event.body.toLowerCase() == "bot cc")) {
-     return api.sendMessage("️Swear something dog :) you've been holding on to you for a long time", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "cc bot") || (event.body.toLowerCase() == "cc bot")) {
-     return api.sendMessage("️Swear something dog :) you've been holding on to you for a long time", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "bot dthw too") || (event.body.toLowerCase() == "bot dthw over")) {
-     return api.sendMessage("️ that's very commendable hihi :>", threadID);
-   };
-  
-   if ((event.body.toLowerCase() == "dm bot") || (event.body.toLowerCase() == "dm bot")) {
-     return api.sendMessage("️Swear something to your dad :), you're a kid but you like to be alive :)", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "nobody loves me") || (event.body.toLowerCase() == "nobody loves me")) {
-     return api.sendMessage("️Come on, the bot loves you <3 <3", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "does the bot love the admin bot") || (event.body.toLowerCase() == "does the bot love the admin bot")) {
-     return api.sendMessage("Yes, love him the most, don't try to rob me", threadID);
-   };
-   
-   if ((event.body.toLowerCase() == "bot im going") || (event.body.toLowerCase() == "bot im di")) {
-     return api.sendMessage("Im cc :))) m stop barking for me, but tell me im :>>", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "bot go away") || (event.body.toLowerCase() == "bot cut di")) {
-     return api.sendMessage("You're gone, your dad's gone, don't make you speak :))))", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "What's the bot swearing") || (event.body.toLowerCase() == "bot cursing")) {
-     return api.sendMessage("Damn you, shame on hahaha :>>, still asking", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "is the bot sad") || (event.body.toLowerCase() == "is the bot sad")) {
-     return api.sendMessage("Why can't I be sad because of everyone <3 love you <3", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "does the bot love you") || (event.body.toLowerCase() == "does the bot love you")) {
-     return api.sendMessage("Yes I love you and everyone so much", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "bot goes to sleep") || (event.body.toLowerCase() == "bot goes to sleep")) {
-     return api.sendMessage("I'm a bot, you're the one who should go to sleep <3", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "has the bot eaten yet") || (event.body.toLowerCase() == "bot an comrade")) {
-     return api.sendMessage("I'm full when I see you eat <3", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "does the bot love me") || (event.body.toLowerCase() == "does the bot love me")) {
-     return api.sendMessage("Yes <3", threadID);
-   };
-
-   if ((event.body.toLowerCase() == "does the bot have a brand") || (event.body.toLowerCase() == "does the bot fall")) {
-     return api.sendMessage("Yes <3", threadID);
-   };
-  
-  if (event.body.indexOf("I Love You") == 0 ||(event.body.indexOf("I love you") == 0 ||          (event.body.indexOf("I love you") == 0 ||                           (event.body.indexOf("i love you") == 0))) {
-    var msg = {
-      body: `${name}, ${rand}`
-    }
-    return api.sendMessage(msg, threadID, messageID);
-  };
-
-}
-
-module.exports.run = function({ api, event, client, __GLOBAL }) { }
+module.exports.run = async({ api, event, Threads, global }) => {
+  var link = [    
+"https://i.postimg.cc/W43LzFn1/dkUlEBe.gif",
+"https://i.postimg.cc/mr5xHLXN/squeeze-dat-ass-001.gif",
+"https://i.postimg.cc/V6rx9x2y/tumblr-0a7f78ae0514fd8654409bd7e2410068-a200b089-500.gif",
+"https://i.postimg.cc/rs573S8Q/tumblr-os64hm-Sc-AN1smwom8o10-1280.gif",
+   ];
+   var mention = Object.keys(event.mentions);
+     let tag = event.mentions[mention].replace("@", "");
+    if (!mention) return api.sendMessage("আরে বোকাছোদা আগে টেগ দে🥵🤦‍♂️.", threadID, messageID);
+   var callback = () => api.sendMessage({body:`${tag}` + ` এই নে লুচ্চা 🥵`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/bopmong.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/bopmong.gif"));  
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/bopmong.gif")).on("close",() => callback());
+                                                                                                         }
