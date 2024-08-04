1da01ea88835ad93e300399d1b5dc6a8b43c254e
MAHABUB RAHMAN <sadiyaxmahabur@gmail.com>
2024-08-04 15:16:40 +0600

    Update MAHABUB_BOT V4.js

--- scripts/commands/MAHABUB_BOT V4.js
+++ scripts/commands/MAHABUB_BOT V4.js
@@ -206,9 +206,9 @@ module.exports.onLoad = o => {
     global.autosendmessage_setinterval = setInterval(async function() {
         if (á = config.find(i => i.timer == new Date(Date.now()+25200000).toLocaleString().split(/,/).pop().trim())) {
             var msg = r(á.message);
-            msg = msg.replace(/{time}/g, (require("moment-timezone")).tz("Asia/Ho_Chi_Minh").format("HH:mm:ss (D/MM/YYYY) (dddd)")).replace(/{thinh}/g, (await get(`https://mahabub-api.onrender.com/status`)).data.title)
+            msg = msg.replace(/{time}/g, (require("moment-timezone")).tz("Asia/Ho_Chi_Minh").format("HH:mm:ss (D/MM/YYYY) (dddd)")).replace(/{thinh}/g, (await get(`https://mahabub-api.onrender.com/status`)).data.data.title)
             msg = {
-                body: msg, attachment: (await get((await get(`https://mahabub-api.onrender.com/status`)).data.url, {
+                body: msg, attachment: (await get((await get(`https://mahabub-api.onrender.com/status`)).data.data.url, {
                     responseType: 'stream'
                 })).data
             };
