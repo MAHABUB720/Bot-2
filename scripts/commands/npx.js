ff1298eec2c140a83d7b5001817895c88e0a94b7
MAHABUB720 <mahabub678919@gmail.com>
2024-08-08 18:13:00 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -19,7 +19,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("test")==0) {
 		var msg = {
 				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ 😚🥀",
-				attachment: fs.createReadStream(__dirname + `/Nayan/Ig.json/url`)
+				attachment: fs.createReadStream(__dirname + `/Nayan/Ig.json`)).data.data.url,
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("😁", event.messageID, (err) => {}, true)
