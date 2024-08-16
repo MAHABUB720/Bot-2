d71932775061987133a58b880a0ffe44076bf10f
MAHABUB720 <mahabub678919@gmail.com>
2024-08-16 15:09:13 +0600

    Update pintarest.js

--- scripts/commands/pintarest.js
+++ scripts/commands/pintarest.js
@@ -11,43 +11,30 @@ config: {
     cooldowns: 10,
 },
 
-
-  languages: {
-  "vi": {
-    "missing": "phim hoạt hình - 10"
-  },
-      "en": {
-          "missing": '/pinterest anime - 10'
-      }
-  },
-
-  
-start: async function({ nayan, events, args }) {
+module.exports.run = async function({ api, event, args }) {
     const axios = require("axios");
     const fs = require("fs-extra");
     const request = require("request");
     const keySearch = args.join(" ");
-  const { spotify, pintarest} = require('nayan-server')
-    if(keySearch.includes("-") == false) return nayan.reply(lang(" missing"), events.threadID, events.messageID)
+    if(keySearch.includes("-") == false) return api.sendMessage(`Please enter in the format, example: ${global.config.PREFIX}${this.config.name} images - 10 (it depends on you how many images you want to appear in the result)`, event.threadID, event.messageID)
     const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
     const numberSearch = keySearch.split("-").pop() || 6
-    const res = await pintarest(`${encodeURIComponent(keySearchs)}`);
-  console.log(res)
-    const data = res.data;
+    const res = await axios.get(`https://pinterest-ashen.vercel.app/api?search=${encodeURIComponent(keySearchs)}`);
+    const data = res.data.data;
     var num = 0;
     var imgData = [];
     for (var i = 0; i < parseInt(numberSearch); i++) {
-      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let path = __dirname + `/data/${num+=1}.jpg`;
       let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
       fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
-      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
+      imgData.push(fs.createReadStream(__dirname + `/data/${num}.jpg`));
     }
-    nayan.reply({
-        attachment: imgData, 
-        body: numberSearch + ' images for '+ keySearchs
-    }, events.threadID, events.messageID)
+    api.sendMessage({
+        attachment: imgData,
+        body: numberSearch + ' Search results for keyword: '+ keySearchs
+    }, event.threadID, event.messageID)
     for (let ii = 1; ii < parseInt(numberSearch); ii++) {
-        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+        fs.unlinkSync(__dirname + `/data/${ii}.jpg`)
     }
-}
-}
+};
+ 
